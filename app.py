8c667896c8ed7c616e90e8eb3070fb5524f36900
Shreyansh <shreyanshsharma0288@gmail.com>
2026-06-16 22:49:08 +0530

    Day 3: Added college profile pages and SQLite integration

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/colleges")
 def college_page():
     return render_template("colleges.html")
+
 
 @app.route("/search", methods=["POST"])
 def search():
@@ -37,6 +40,28 @@ def search():
         "results.html",
         colleges=colleges
     )
+
+
+@app.route("/college/<int:college_id>")
+def college_profile(college_id):
+
+    conn = sqlite3.connect("college.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM colleges WHERE id = ?",
+        (college_id,)
+    )
+
+    college = cursor.fetchone()
+
+    conn.close()
+
+    return render_template(
+        "college_profile.html",
+        college=college
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
